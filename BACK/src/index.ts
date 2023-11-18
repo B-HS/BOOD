520f15d76c911e247fa99920cedadbaefe5fbedf
Hyunseok Byun <gumyoincirno@gmail.com>
2023-11-18 14:43:29 +0900

    [BACK] Add initialize admin/prisma logic

--- BACK/src/index.ts
+++ BACK/src/index.ts
@@ -1,18 +1,23 @@
-import cors from "cors";
-import dotenv from "dotenv";
-import express, { Express } from "express";
-import { initRoutes } from "./routes/routes";
-dotenv.config();
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express, { Express } from 'express'
+import { initCronJob } from './cron/cron'
+import { initFirebaseAdmin } from './firebase/firebase'
+import { initRoutes } from './routes/routes'
+dotenv.config()
 
-const app: Express = express();
-const PORT = process.env.PORT;
+const app: Express = express()
+const PORT = process.env.PORT
+const admin = initFirebaseAdmin()
+const prisma = new PrismaClient()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-initRoutes(app);
-
+initRoutes(app, prisma)
+initCronJob(prisma, admin)
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+})
