d6b7cf6892a95bbe6454335b6cee37ee4a816d4d
Hyunseok Byun <gumyoincirno@gmail.com>
2024-01-21 17:16:50 +0900

    Remove all files

--- FRONT/src/app/providers.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import theme from '@/config/theme'
-import { ChakraProvider } from '@chakra-ui/react'
-
-const Providers = ({ children }: { children: React.ReactNode }) => {
-    return <ChakraProvider theme={theme}>{children}</ChakraProvider>
-}
-
-export default Providers
--- README.md
+++
@@ -1,2 +0,0 @@
-# BOOD
-- SUPABASE(개인서버) + Nextjs로 재작성 예정
